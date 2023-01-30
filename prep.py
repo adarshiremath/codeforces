65a92db6b3a0bf864358dad43fad3017d6422765
adarshiremath <54905828+adarshiremath@users.noreply.github.com>
2023-01-29 18:23:40 -0700

    Delete prep.py

--- prep.py
+++
@@ -1,8 +0,0 @@
-import os
-with open('./test.txt') as f:
-    lines = f.readlines()
-    lines = lines[0].strip().split("  ")
-    dir = "./Contest-" + lines[1] + "/"
-    os.mkdir(dir)
-    f1 = open(dir+"_".join(lines[0].split(" ")).strip()+".py","x")
-    f1.close()
